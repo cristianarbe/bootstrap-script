7a56ce3c7e0fa0290853b0c7cc62e0350d0bc178
cristianarbe <c.ariza@posteo.de>
2019-06-09 12:07:21 +0100

    Add logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
+
+[[ -f log ]] && rm log
 
 if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root"
   exit 1
 fi
+
 readonly REPO="https://github.com/cristianarbe/dot-files.git"
 readonly PIA_EXISTS=$(find /etc/openvpn/ -name "pia*" | wc -l)
 readonly PIA_URL="https://www.privateinternetaccess.com/installer/pia-nm.sh"
@@ -15,13 +18,13 @@ dot_files(){
   else
     echo "###### Setting up dot files"
     cd  /tmp/ || exit
-    git clone "$REPO"
+    git clone "$REPO" >> log
     cd dot-files || exit
     shopt -s dotglob
-    mv /tmp/dot-files/* "/home/${SUDO_USER}/"
+    mv -v /tmp/dot-files/* "/home/${SUDO_USER}/" >> log
   fi
-  mkdir -p /usr/share/themes/noborders/xfwm4/
-  touch /usr/share/themes/noborders/xfwm4/themerc
+  mkdir -vp /usr/share/themes/noborders/xfwm4/ >> log
+  touch /usr/share/themes/noborders/xfwm4/themerc >> log
 }
 
 install_dnf(){
@@ -39,40 +42,39 @@ extra_packages(){
   if [[ $PIA_EXISTS -eq 0 ]]; then
     cd /tmp/ || exit
     echo "###### Installing PIA"
-    wget "$PIA_URL"
+    wget "$PIA_URL" >> log
     bash pia-nm.sh
   else
     echo "PIA is already installed"
   fi
 
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> log
 
   cd /tmp/ || exit
   if [[ -f /bin/megasync ]]; then
     echo "MEGASync is already installed"
   else
     echo "##### Installing MEGASync"
-    wget $MEGASYNC_URL
+    wget $MEGASYNC_URL >> log
     local file
     file=$(find . -name "megasync-Fedora*")
-    dnf install "$file"
+    dnf install "$file" -y
   fi
 
-    wget 'https://dl.opendesktop.org/api/files/download/id/1460968153/s/630a5ea1c93c05cefad04f3c4fd89059f9ef6112b2d50f27ed217a6a9464a439c2f91480327da21eb5c9954f4d4a1c3172d43510103b96d00752b60b42197ac4/t/1560079934/lt/download/162986-waldorf1314.tar.xz'
-    tar xf 162986-waldorf1314.tar.xz
-    cp waldorf1314 /usr/share/themes/ -r
+    wget 'https://dl.opendesktop.org/api/files/download/id/1460968153/s/630a5ea1c93c05cefad04f3c4fd89059f9ef6112b2d50f27ed217a6a9464a439c2f91480327da21eb5c9954f4d4a1c3172d43510103b96d00752b60b42197ac4/t/1560079934/lt/download/162986-waldorf1314.tar.xz' >> log
+    tar xf 162986-waldorf1314.tar.xz >> log
+    cp waldorf1314 /usr/share/themes/ -vr >> log
   }
 
 setup_openbox(){
   dnf install openbox xbacklight feh xorg-x11-drv-libinput tint2 \
-    volumeicon xorg-x11-server-utils network-manager-applet
+    volumeicon xorg-x11-server-utils network-manager-applet -y
     }
 
 
   function main(){
 
-    
     echo ""
     echo "1. Dot files install"
     echo "===================="
@@ -85,7 +87,7 @@ setup_openbox(){
 
     echo ""
     echo "2. Setup openbox"
-    echo "===================="
+    echo "================"
     echo ""
     echo "This sets up openbox"
     read -rp "Do you want to proceed? [y/N]: " response
@@ -105,7 +107,7 @@ setup_openbox(){
 
     echo ""
     echo "4. Extra packages"
-    echo "===================="
+    echo "================="
     echo ""
     echo "This installs packages that are not in dnf. This includes PIA, \
 MegaSync and vim plug"
@@ -117,7 +119,7 @@ MegaSync and vim plug"
 
     echo ""
     echo "5. Installation finished"
-    echo "================="
+    echo "========================"
     echo ""
     echo "This step will reboot the system"
     read -rp "Do you want to proceed? [y/N]: " response
