89702836a16136acec4ab4190057feb8e351eead
cristianarbe <c.ariza@posteo.net>
2019-11-09 13:49:59 +0000

    Check if vim plug is installed

--- test/extra/vim_plug.sh
+++ test/extra/vim_plug.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-su - "${SUDO_USER}" -c "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+if [[ -f /home/${SUDO_USER}/.vim/autoload/plug.vim ]]; then
+    su - "${SUDO_USER}" -c "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+fi
