e2db11b825ba1eb6cd5ad3b74efd0dcfbd7d3337
cristianarbe <c.ariza@posteo.net>
2019-11-09 13:33:45 +0000

    Add check for icecat existing

--- test/extra/icecat.sh
+++ test/extra/icecat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-cd /tmp/ || exit
-wget https://mirrors.ocf.berkeley.edu/gnu/gnuzilla/60.7.0/icecat-60.7.0.en-US.gnulinux-x86_64.tar.bz2
-tar xfj icecat-60.7.0.en-US.gnulinux-x86_64.tar.bz2
-mv  icecat /opt/
-ln -s /opt/icecat/icecat /usr/local/bin/icecat
+if [[ ! -d /opt/icecat ]]; then
+    cd /tmp/ || exit
+    wget https://mirrors.ocf.berkeley.edu/gnu/gnuzilla/60.7.0/icecat-60.7.0.en-US.gnulinux-x86_64.tar.bz2
+    tar xfj icecat-60.7.0.en-US.gnulinux-x86_64.tar.bz2
+    mv  icecat /opt/
+    ln -s /opt/icecat/icecat /usr/local/bin/icecat
+fi
