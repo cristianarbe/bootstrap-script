68dfbf56fb67a6ff91df28a73bd0764600a0b2de
cristianarbe <c.ariza@posteo.net>
2019-11-09 11:07:59 +0000

    Add update first

--- modules/install_apt.sh
+++ modules/install_apt.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 install_apt(){
+    echo 'Updating cache...'
+    apt update
     echo "Installing apt packages..."
     # shellcheck disable=SC2154
     # shellcheck disable=SC2068
-    apt install ${install[@]} -y
+    apt install ${install[@]}
     
     echo "Uninstalling apt packages..."
     # shellcheck disable=SC2154
     # shellcheck disable=SC2068
-    apt remove ${uninstall[@]} -y
+    apt remove ${uninstall[@]}
 }
