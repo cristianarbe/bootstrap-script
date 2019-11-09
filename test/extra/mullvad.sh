40ca7e7d2d52b6cf1eb3b3758ec2bd435f40272c
cristianarbe <c.ariza@posteo.net>
2019-11-09 13:42:19 +0000

    Check if mullvad exists

--- test/extra/mullvad.sh
+++ test/extra/mullvad.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-wget https://mullvad.net/media/app/MullvadVPN-2019.9_amd64.deb
-apt install ./MullvadVPN-2019.9_amd64.deb
+if ! dpkg -l mullvad-vpn; then
+    wget https://mullvad.net/media/app/MullvadVPN-2019.9_amd64.deb
+    apt install ./MullvadVPN-2019.9_amd64.deb
+fi
