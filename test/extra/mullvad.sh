fa14d230592fb6e61da6b2b630d57ae9b036d635
cristianarbe <c.ariza@posteo.net>
2019-11-12 05:32:22 +0100

    Remove unused files

--- test/extra/mullvad.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if ! dpkg -l mullvad-vpn &> /dev/null; then
-    wget https://mullvad.net/media/app/MullvadVPN-2019.9_amd64.deb
-    apt install ./MullvadVPN-2019.9_amd64.deb
-else
-    echo 'Mullvad is already installed.'
-fi
